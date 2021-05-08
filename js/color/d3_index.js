cd549ce3b8d0afac32885d532be7e06df7be9f06
Aaron Kangas <batman080580@gmail.com>
2021-05-07 22:09:17 -0700

    deletes/renames

--- js/color/d3_index.js
+++
@@ -1,6 +0,0 @@
-export { default as color, rgb, hsl }
-    from "./d3_color.js";
-export { default as lab, hcl, lch, gray }
-    from "./d3_lab.js";
-export { default as cubehelix }
-    from "./d3_cubehelix.js";
